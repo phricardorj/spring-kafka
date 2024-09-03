c87570a16dc97823a0d75f09f29d44db6d418b18
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2024-09-02 23:33:52 -0300

    chore: update HelloConsumer.java

--- src/main/java/br/com/phricardo/messaging/services/HelloConsumer.java
+++ src/main/java/br/com/phricardo/messaging/services/HelloConsumer.java
@@ -9,6 +9,8 @@ public class HelloConsumer {
 
     private final static String TOPIC_NAME = "hello-topic";
 
+    // O Kafka particiona os tópicos, e os consumidores de um mesmo grupo (groupId) podem ler diferentes partições do mesmo tópico.
+    // A carga de mensagens é distribuída entre os consumidores, permitindo o processamento paralelo.
     @KafkaListener(topics = TOPIC_NAME, groupId = "group-1")
     private void messageReceiver(String message) {
         System.out.println(message);
